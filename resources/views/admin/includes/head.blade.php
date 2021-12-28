b185f6ad5a9e241dd550f52e83c06f05adbe444a
prakashjay13 <90688413+prakashjay13@users.noreply.github.com>
2021-12-28 20:16:57 +0530

    Update head.blade.php

--- resources/views/admin/includes/head.blade.php
+++ resources/views/admin/includes/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Asset Tracker | Dashboard</title>
+    <title>Ecomm-Application | Dashboard</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback')}}">
